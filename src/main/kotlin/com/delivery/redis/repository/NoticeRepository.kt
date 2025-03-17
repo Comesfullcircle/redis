5a3ce7095e5cfbfd14260491f7088d842f9dc6b4
윤설이 <olerlmin1234@gmail.com>
2025-03-17 15:56:39 +0900

    redis

---
+++ src/main/kotlin/com/delivery/redis/repository/NoticeRepository.kt
@@ -0,0 +1,31 @@
+package com.delivery.redis.repository
+
+import com.delivery.redis.common.Log
+import com.delivery.redis.model.NoticeDto
+import org.springframework.stereotype.Service
+
+@Service
+class NoticeRepository {
+
+    companion object: Log
+
+    private val noticeList = mutableListOf<NoticeDto>()
+
+    fun getNotice(id: Long?): NoticeDto? {
+        log.info("repository get notice method call : {}", id)
+        return noticeList.filter { it -> it.id == id }.firstOrNull()
+    }
+
+    fun addNotice(notice: NoticeDto?): NoticeDto? {
+        log.info("repository add notice method call: {}", notice)
+
+        return notice?.apply {
+            id = (noticeList.size+1).toLong()
+        }?.apply{
+            noticeList.add(this)
+        }?.also{
+            log.info("save dto: {}", it)
+        }
+
+    }
+}
